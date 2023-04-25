61e096eb5e1a991752e294f5991e82a8a7e56de4
GurinAOA <kiulo.oksana@gmail.com>
2023-04-25 20:21:29 +0300

    add homework js lesson2

---
+++ les.js
@@ -0,0 +1,14 @@
+// let country = 'Sweden';
+// let access = (country == 'Sweden') ? ('true'): ('false');
+// console.log(access);
+
+let a =10;//  задание 2;
+a++;
+console.log(a);
+
+
+
+let b=10;
+for (let b = 10; b <11; b++) {
+  console.log(b);
+}
